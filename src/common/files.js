65bbbe992c554a03b1bbeffa0cd6cbd40d6ec056
Zachary Waldowski <zach@waldowski.me>
2020-01-08 01:46:25 -0500

    feat(earmark): remove make-dir dependency, clean up fs promises

--- src/common/files.js
+++ src/common/files.js
@@ -1,14 +1,7 @@
-import fs from 'fs'
-import mkdir from 'make-dir'
+import { promises as fs } from 'fs'
 import path from 'path'
-import util from 'util'
 import vfile from 'to-vfile'
 import { reject } from './errors'
-
-const readFileEx = util.promisify(fs.readFile)
-const writeFileEx = util.promisify(fs.writeFile)
-const readdirEx = util.promisify(fs.readdir)
-const stat = util.promisify(fs.stat)
 
 export const createPath = (...args) =>
   process.env.NODE_ENV === 'test'
@@ -16,9 +9,9 @@ export const createPath = (...args) =>
     : path.resolve(__dirname, '..', 'data', ...args)
 
 export const readFile = (filename, ...args) =>
-  readFileEx(filename, ...args).catch(error =>
-    reject(`Error reading ${filename}`, error)
-  )
+  fs
+    .readFile(filename, ...args)
+    .catch(error => reject(`Error reading ${filename}`, error))
 
 export const readVFile = filename =>
   vfile
@@ -26,17 +19,20 @@ export const readVFile = filename =>
     .catch(error => reject(`Error reading ${filename}`, error))
 
 export const writeFile = (filename, ...args) =>
-  mkdir(path.dirname(filename))
-    .then(() => writeFileEx(filename, ...args))
+  fs
+    .mkdir(path.dirname(filename), { recursive: true })
+    .then(() => fs.writeFile(filename, ...args))
     .catch(error => reject(`Error writing to ${filename}`, error))
 
 export const readdir = dir =>
-  readdirEx(dir)
+  fs
+    .readdir(dir)
     .then(subdirs =>
       Promise.all(
         subdirs.map(subdir => {
           const res = path.join(dir, subdir)
-          return stat(res)
+          return fs
+            .stat(res)
             .then(stats => stats.isDirectory())
             .then(ret => (ret ? readdir(res) : res))
         })
